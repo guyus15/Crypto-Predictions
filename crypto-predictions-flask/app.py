915630bb56e7f0bca80ea55096016430ef4a1f79
David Kidd <32614126+Davidk20@users.noreply.github.com>
2021-02-06 21:22:29 +0000

    data jsonified

--- crypto-predictions-flask/app.py
+++ crypto-predictions-flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from prices import Prices
 
 app = Flask(__name__)
@@ -8,14 +8,14 @@ def get_info():
         currency = request.args.get('currency')
         print("Currency: " + currency)
         currency_info = Prices(currency)
-        return currency_info.get_info()
+        return jsonify(currency_info.get_info())
 
 @app.route('/getprice')
 def get_price():
         currency = request.args.get('currency')
         print("Currency: " + currency)
         current_price = Prices(currency)
-        return current_price.get_price()
+        return jsonify(current_price.get_price())
 
 if __name__ == "__main__":
   app.run()
