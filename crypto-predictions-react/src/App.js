bc86dce3293fc9c7ab9d76eb79c8b246c39de594
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2021-02-06 16:35:23 +0000

    Add CurrentPrice component

--- crypto-predictions-react/src/App.js
+++ crypto-predictions-react/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DataBox from './Components/DataBox';
-
+import CurrentPrice from './Components/CurrentPrice'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App-container">
+          <CurrentPrice name="Bitcoin (BTC)" price="27500"></CurrentPrice>
+          <div style={{height: '38%'}}></div> {/*Placeholder div to represent the graph*/}
           <div className="Databoxes">
             <DataBox title="Watchlist" type="watchlist"></DataBox>
             <DataBox title="Trending Stories" type="news"></DataBox>
---
+++ crypto-predictions-react/src/Components/CurrentPrice.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class CurrentPrice extends React.Component {
+  render() {
+    return (
+      <div className="CurrentPrice-container">
+        <p className="CurrentPrice-name">{this.props.name}</p>
+        <p className="CurrentPrice-price">£{this.props.price}</p>
+        <p className="CurrentPrice-text">Current Price</p>
+      </div>
+    )
+  }
+}
+
+export default CurrentPrice
