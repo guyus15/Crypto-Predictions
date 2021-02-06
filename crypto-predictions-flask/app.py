2b6fc31beab245cacd88e74724a850d8a0d76d6a
David Kidd <32614126+Davidk20@users.noreply.github.com>
2021-02-06 22:20:35 +0000

    sqlalchemy created

--- crypto-predictions-flask/app.py
+++ crypto-predictions-flask/app.py
@@ -1,7 +1,26 @@
 from flask import Flask, request, render_template, jsonify
 from prices import Prices
+from flask_sqlalchemy import SQLAlchemy
+
+ 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///crypto_data.db"
+app.config["SECRET_KEY"] = "SECRETKEY"
+db = SQLAlchemy(app)
+
+class currency_info(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    symbol = db.Column(db.String(5), unique=True)
+    logo = db.Column(db.String(30))
+    description = db.Column(db.String(300))
+    last_updated = db.Column(db.String(20))
+    date_added = db.Column(db.String(20))
+    website = db.Column(db.String(30))
+    technical_doc = db.Column(db.String(30))
+
+
 
 @app.route('/getinfo')
 def get_info():
