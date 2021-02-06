d345e26fbf2fe5fe208eb47b777e60611651ce34
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2021-02-06 19:39:33 +0000

    Create graph with test data

--- crypto-predictions-react/src/App.js
+++ crypto-predictions-react/src/App.js
@@ -3,13 +3,63 @@ import logo from './logo.svg';
 import './App.css';
 import DataBox from './Components/DataBox';
 import CurrentPrice from './Components/CurrentPrice'
+import Graph from './Components/Graph'
+
+let timeSeries = {
+  "title": "Visits",
+  "data": [
+    {
+      "time": "Tue May 01 2018 00:00:00 GMT+0100 (British Summer Time)",
+      "value": 39
+    },
+    {
+      "time": "Wed May 02 2018 00:00:00 GMT+0100 (British Summer Time)",
+      "value": 60
+    },
+    {
+      "time": "Wed June 02 2018 00:00:00 GMT+0100 (British Summer Time)",
+      "value": 60
+    },
+    {
+      "time": "Wed August 02 2018 00:00:00 GMT+0100 (British Summer Time)",
+      "value": 60
+    }
+  ]
+}
+
+let categoricalData = {
+  "title": "Categories",
+  "data": [
+    {
+      "label": "A",
+      "value": 46
+    },
+    {
+      "label": "B",
+      "value": 87
+    },
+    {
+      "label": "C",
+      "value": 112
+    },
+    {
+      "label": "D",
+      "value": 190
+    },
+  ]
+}
+
 
 class App extends React.Component {
   render() {
     return (
       <div className="App-container">
           <CurrentPrice name="Bitcoin (BTC)" price="27500"></CurrentPrice>
-          <div style={{height: '38%'}}></div> {/*Placeholder div to represent the graph*/}
+          <Graph
+            data={categoricalData}
+            title="Test graph title"
+            color="#70CAD1"
+          />
           <div className="Databoxes">
             <DataBox title="Watchlist" type="watchlist"></DataBox>
             <DataBox title="Trending Stories" type="news"></DataBox>
--- crypto-predictions-react/src/Components/Graph.js
+++ crypto-predictions-react/src/Components/Graph.js
@@ -8,14 +8,27 @@ class Graph extends React.Component {
   }
 
   componentDidMount() {
-    this.myChart = new Chart(this.canvasRef.current, {
-      type: 'bar'
+
+    console.log(this.props.data.data);
+    let dataLabels = this.props.data.data.map(d => d.label);
+    let dataValues = this.props.data.data.map(d => d.value);
+
+    this.myChart = new Chart(this.chartRef.current, {
+      type: 'bar',
+      data: {
+        labels:dataLabels,
+        datasets: [{
+          label: this.props.title,
+          data: dataValues,
+          backgroundColor: this.props.color
+        }]
+      }
     });
   }
 
   render() {
     return (
-      <canvas />
+      <canvas ref={this.chartRef} />
     )
   }
 }
