3b3ccf5698905fa5c1995c9c9f8cd2807589c3cb
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2021-02-06 21:18:35 +0000

    Add new roots to flask

--- crypto-predictions-flask/app.py
+++ crypto-predictions-flask/app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 from prices import Prices
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-        bitcoin = Prices("bitcoin")
-        return bitcoin.get_info()
+@app.route('/getinfo')
+def get_info():
+        currency = request.args.get('currency')
+        print("Currency: " + currency)
+        currency_info = Prices(currency)
+        return currency_info.get_info()
+
+@app.route('/getprice')
+def get_price():
+        currency = request.args.get('currency')
+        print("Currency: " + currency)
+        current_price = Prices(currency)
+        return current_price.get_price()
 
 if __name__ == "__main__":
   app.run()
